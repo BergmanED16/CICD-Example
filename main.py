b81b9252b95ec148cd9d264d57446fb2e40f1398
BergmanED16 <nahte.bergman@gmail.com>
2025-09-30 14:47:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,4 @@ app = Flask(__name__)
 def hello_world():
 <<<<<<< HEAD
     return "Hello, World! The CI/CD Pipeline Worked! Version 66.0"
-=======
-    return "Hello, DevOps Students! Version 3.0"
->>>>>>> df6316e6a98da1c805a53fccd616404e6eb36af1
+
