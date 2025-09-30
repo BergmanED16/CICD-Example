00c8d051032ba98b61309eafe230a7425385322d
BergmanED16 <nahte.bergman@gmail.com>
2025-09-30 14:53:17 -0500

    Update main.py with new code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# main.py
-
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-<<<<<<< HEAD
     return "Hello, World! The CI/CD Pipeline Worked! Version 66.0"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run sets $PORT
+    app.run(host="0.0.0.0", port=port)
 
